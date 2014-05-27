c7c67998dc5124fc28102fe721e6fda5a566772a
manikandan <manikandan.m@npcompete.com>
2014-05-27 09:50:54 +0530

    Initial commit : Tattle control with example project

---
+++ Example/Classes/AppDelegate.m
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.m
+//  Tattle-UI
+//
+//  Created by Mani on 5/26/14.
+//  Copyright (c) 2014 Tattle. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "TattleControl.h"
+#import "FirstVC.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [[TattleControl sharedControl] enableTattleToWindow:self.window];
+    
+    [[TattleControl sharedControl] assignRecipientEmailId:@"yourmailid@domain.com" withCCId:@"yourmailid@domain.com" emailSubject:@"Bugs"];
+    /* Optional Configuration
+     
+     //To Add Recipients for mail
+     [[TattleControl sharedControl] addRecipientMailId:@"yourmailid@domain.com"];
+     
+     //To set CC for mail:
+     [[TattleControl sharedControl] addCCMailId:@"yourmailid@domain.com"];
+     
+     //To set Subject for mail:
+     [[TattleControl sharedControl] assignMailSubject:@"Bugs"];
+     
+     //Optionally change colors:
+     //Change Scribble Color according to background, default is black
+     [[TattleControl sharedControl] setScribbleColor:YOUR_Color];
+     
+     //Change background color of movable control. default is black(transparent)
+     [[TattleControl sharedControl] setMovableControlBackgroundColor:YOUR_Color];
+     [[TattleControl sharedControl] setMovableControlBackgroundColor:YOUR_Color withAlpha:alpha];
+
+     */
+    self.window.backgroundColor = [UIColor whiteColor];
+    FirstVC *firstVC = [[FirstVC alloc]init];
+    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:firstVC];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ Example/Classes/FirstVC/FirstVC.m
@@ -0,0 +1,69 @@
+//
+//  FirstVC.m
+/*
+ Copyright (c) 2014 TattleUI (http://www.npcompete.com/)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+#import "FirstVC.h"
+#import "SecondVC.h"
+
+@interface FirstVC ()
+
+@property (nonatomic,weak) IBOutlet UIButton *secondBtn;
+
+-(IBAction)secondButtonFired:(id)sender;
+
+@end
+
+@implementation FirstVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"First";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Second Button Fired 
+
+-(IBAction)secondButtonFired:(id)sender
+{
+    NSLog(@"Second Button Fired");
+    SecondVC *secondVC = [[SecondVC alloc] init];
+    [self.navigationController pushViewController:secondVC animated:YES];
+}
+
+@end
